4d5baacbf415f305e9c1f90321788983d2a1be7f
Anders Ramsay <andersr@gmail.com>
2015-09-05 16:54:02 -0400

    diplay create vote form

--- client/views/votes/voteTitle/voteTitle.js
+++ client/views/votes/voteTitle/voteTitle.js
@@ -8,43 +8,43 @@ Template.voteTitle.helpers({
   }
 })
 
-Template.voteTitle.events({
-  "submit .vote-title-form": function(event,template){
-    //prevent unwanted default behavior for form submit
-    event.preventDefault();
+// Template.voteTitle.events({
+//   "submit .vote-title-form": function(event,template){
+//     //prevent unwanted default behavior for form submit
+//     event.preventDefault();
 
-    //allow for exposing 'this' to internal functions, such as Meteor.call
-    var self = this;
+//     //allow for exposing 'this' to internal functions, such as Meteor.call
+//     var self = this;
 
-    //get text entered into topicTitle field
-    var voteTitle = e.target.voteTitle.value;
+//     //get text entered into topicTitle field
+//     var voteTitle = e.target.voteTitle.value;
 
-    //This will only work with insecure turned on:
-    // var topicId = Topics.insert({
-    //   title: topicTitle,
-    //   updatedAt: Date()
-    // });
+//     //This will only work with insecure turned on:
+//     // var topicId = Topics.insert({
+//     //   title: topicTitle,
+//     //   updatedAt: Date()
+//     // });
 
-    // //Associate a vote with this topic
-    // Votes.insert({
-    //   topicId: topicId,
-    //   voteCount: 0
-    // });
+//     // //Associate a vote with this topic
+//     // Votes.insert({
+//     //   topicId: topicId,
+//     //   voteCount: 0
+//     // });
 
-    var voteAttributes = {
-      title: voteTitle
-    };
+//     var voteAttributes = {
+//       title: voteTitle
+//     };
 
-    Meteor.call('createVote', voteAttributes, function(error, result){
-      if (error){
-        console.log(error.reason);
-      } else {
+//     Meteor.call('createVote', voteAttributes, function(error, result){
+//       if (error){
+//         console.log(error.reason);
+//       } else {
 
-         //clear out form data after insert
-         template.find("form").reset();
+//          //clear out form data after insert
+//          template.find("form").reset();
 
-      }
-    });
+//       }
+//     });
 
-  }
-});
+//   }
+// });
