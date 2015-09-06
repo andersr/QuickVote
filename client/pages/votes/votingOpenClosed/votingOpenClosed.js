eb7e68257cdf3123b4451d732cdbaf6f0f77b687
Anders Ramsay <andersr@gmail.com>
2015-09-06 16:11:44 -0400

    vote open close toggle

--- client/pages/votes/votingOpenClosed/votingOpenClosed.js
+++ client/pages/votes/votingOpenClosed/votingOpenClosed.js
@@ -1,6 +1,38 @@
+Template.votingOpenClosed.onRendered(function(){
+   this.$('.voteOpenClose').bootstrapToggle();
+});
+
 Template.votingOpenClosed.helpers({
-  voteStatus: function(){
-    var currentVote = Vote.findOne({_id: this.params._id});
-    return false;
+  isOpen: function(){
+    return "checked";
   }
 });
+
+
+Template.votingOpenClosed.events({
+  "change .voteOpenClose": function(event){
+    var checked = $(event.target).is(':checked');
+    if (checked) {
+      Session.set("votingOpen", true);
+    } else {
+      Session.set("votingOpen", false);
+    };
+
+
+    // console.log(checked);
+  }
+});
+
+
+// 'click .selectPill': function(e) {
+//     var name = $(e.target).attr('for');
+//     if ($("input[name='+name+']").attr('checked') == 'checked'){
+//       $("input[name='+name+']").trigger('change').removeAttr('checked');
+//     }
+//     else{
+//       $("input[name='+name+']").trigger('change').attr('checked', 'checked');
+//     }
+//   },
+//   'change #input':function(e){
+//     alert("value changed");
+//   }
