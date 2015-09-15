619a65e459a0b5c9dc120af24b148b1fcd3c8492
Anders Ramsay <andersr@gmail.com>
2015-09-15 19:07:51 -0400

    updating vote count in progress

--- client/views/qVote/voteChoices/upDownVote/upDownVote.js
+++ client/views/qVote/voteChoices/upDownVote/upDownVote.js
@@ -1,32 +1,30 @@
 Template.upDownVote.onCreated(function(){
 
-  // this.isUpVote = new ReactiveVar(false);
+  this.isUpVote = new ReactiveVar();
+  this.currentVoteChoiceId = new ReactiveVar(this.data._id);
 
   if(Meteor.userId()){
 
     var currentUserVote = UserVotes.findOne({voteChoiceId: this.data._id, voterId: Meteor.userId() });
       
-
-      this.isUpVote = new ReactiveVar(currentUserVote.upVote);
+    this.isUpVote.set(currentUserVote.upVote);
 
   } else {
 
-    //assume they have not voted and set all votes to default
-    this.isUpVote = new ReactiveVar(false);
+    this.isUpVote.set(false);
 
   };
-
-
 
 });
 
 Template.upDownVote.helpers({
-  // currentUserVote: function(){
 
-   
+  currentVote: function(){
 
+    return Template.instance().isUpVote.get()
 
-  // },
+  },
+
   thumbIconToggle: function(){
 
     if (Template.instance().isUpVote.get()) {
@@ -39,52 +37,114 @@ Template.upDownVote.helpers({
 
   upDownVoteMsg: function(){
 
-    // if (Template.instance().currentVote.get() === "upVote") {
-    //   return "Upvote this choice";
-    // } else {
-    //   return "Undo your upvote";
-    // };
-
+    if (Template.instance().isUpVote.get()) {
+      return "Upvote this choice";
+    } else {
+      return "Undo your upvote";
+    };
   }
+  // , 
+
+  // upDownVoteMsg: function(){
+
+  //   if (Template.instance().isUpVote.get()) {
+  //     return "Upvote this choice";
+  //   } else {
+  //     return "Undo your upvote";
+  //   };
+  // }, 
 
 
 });
 
 Template.upDownVote.events({
 
-  "click .up-down-vote":function(){
-    //user must be signed in to vote...
-    if (Meteor.userId()) {
-      var currentTemplate = Template.instance();
-      var upDownVote = Template.instance().currentVote.get();
-      var upVote;
-      var nextVote;
+  "click .toggle-up-down-vote":function(event, template){
+    var currentVote =  this.currentVote;
+    template.currentVote.set(!currentVote);
 
-      if(upDownVote === 'up'){
-        upVote = true;
-        nextVote = 'down';
-      } else {
-        upVote = false;
-        nextVote = 'up';
-      };
+    // if (!Meteor.userId()) {
 
-      var voteChoiceAttributes = {
-        upVote:upVote,
-        voteChoiceId: this._id
-      }
+    //    //user must be signed in to vote..
+    //    console.log("please sign in to vote...");
+    //    // $('#loginModal').modal('show');
+    // } else {
 
-      Meteor.call('updateVoteCount', voteChoiceAttributes, function (error, result) {
-        if (error){
-          console.log(error.reason);
-        } else {
-          currentTemplate.currentVote.set(nextVote);
-        }
+    //   // console.log("vote choice id: " + Template.instance().currentVoteChoiceId.get());
+    //   // console.log("Is upvote: " + Template.instance().isUpVote.get());
+    //   // var upDownVote = Template.instance().isUpVote.get();
+    
+    //   var voteChoiceAttributes = {
+    //     voteChoiceId: Template.instance().currentVoteChoiceId.get(),
+    //     upVote: Template.instance().isUpVote.get()
+    //   };
 
-      });
+    //   var templateInstance = Template.instance();
 
-    } else {
-       $('#loginModal').modal('show');
-    };
+    //   Meteor.call('updateVoteCount', voteChoiceAttributes, function (error, result) {
 
-  }
+    //     if (error){
+    //       console.log(error.reason);
+    //     } else {
+    //       // console.log(thisTemplate);
+    //     }
+    //     // else {
+
+    //     //   // console.log('template instance: ' + templateInstance);
+    //     //   // console.log('upvote result: ' + result.upVote);
+
+    //     //   // templateInstance.isUpvote.set(result.upVote);
+    //     // }
+    //   });
+
+      // Template.instance().isUpvote.set(!upDownVote);
+      
+    }
+  // }
 });
+
+  
+
+
+    // 1. update the vote count
+    // 2. update this user's userVote
+
+    // var currentTemplate = Template.instance();
+    // var upDownVote = Template.instance().currentVote.get();
+    // var upVote;
+    // var nextVote;
+
+    // if(upDownVote === 'up'){
+    //   upVote = true;
+    //   nextVote = 'down';
+    // } else {
+    //   upVote = false;
+    //   nextVote = 'up';
+    // };
+
+
+
+
+    //   var voteChoiceAttributes = {
+    //     upVote: this.isUpVote.get(),
+    //     voteChoiceId: this.data._id
+    //   }
+
+    //   var voteChoiceAttributes = {
+    //     upVote:upVote,
+    //     voteChoiceId: this._id
+    //   }
+
+    //   Meteor.call('updateVoteCount', voteChoiceAttributes, function (error, result) {
+    //     if (error){
+    //       console.log(error.reason);
+    //     } else {
+    //       currentTemplate.currentVote.set(nextVote);
+    //     }
+
+    //   });
+
+    // } else {
+
+    // };
+
