eaec58808e2ac280be604b123ddd9323f410e5fb
Anders Ramsay <andersr@gmail.com>
2015-09-15 10:10:08 -0400

    remove vote choice

--- client/views/qVote/voteChoices/deleteVoteChoice/deleteVoteChoice.js
+++ client/views/qVote/voteChoices/deleteVoteChoice/deleteVoteChoice.js
@@ -1,10 +1,11 @@
   Template.deleteVoteChoice.events({
     "click .delete-vote-choice":function(){
+      // console.log(this.currentVoteChoiceId);
 
     var confirmDelete = confirm("Really delete this vote choice?");
 
     if(confirmDelete){
-      Meteor.call('removeVoteChoice', this._id, function(error, result){
+      Meteor.call('removeVoteChoice', this.currentVoteChoiceId, function(error, result){
         if (error){
           console.log(error.reason);
         }
--- client/views/qVote/voteChoicesList/voteChoicesList.js
+++ client/views/qVote/voteChoicesList/voteChoicesList.js
@@ -1,5 +1,4 @@
 Template.voteChoicesList.helpers({
-
   voteChoices:function(){
     return VoteChoices.find({voteId: Router.current().params._id }, {sort: {updatedAt: -1}});
   }
