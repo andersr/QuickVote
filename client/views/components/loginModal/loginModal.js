2e3b3134ba74895ae9d06dc88183e5a597e28d4f
Anders Ramsay <andersr@gmail.com>
2015-09-17 11:39:52 -0400

    cleanup

--- client/views/components/loginModal/loginModal.js
+++ client/views/components/loginModal/loginModal.js
@@ -5,14 +5,7 @@ Template.loginModal.onCreated(function(){
     if (Session.get("loginViaModal") && Meteor.userId()) {
       $('#loginModal').modal('hide');
       Session.set("loginViaModal", false);
-      console.log("closing modal...");
     };
-    
+
   });
 });
-
-// Template.loginModal.events({
-//   "click .at-login": function(){
-//    
-//   };
-// });
