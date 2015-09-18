b82ccbb8c3b2dfd6428c4791f66dddec8e189ef0
Anders Ramsay <andersr@gmail.com>
2015-09-18 15:57:45 -0400

    votes collection rules

--- server/publications.js
+++ server/publications.js
@@ -10,3 +10,22 @@ Meteor.publish('userVotes', function() {
   // return UserVotes.find(); 
   return UserVotes.find({voterId: this.userId}); 
 });
+
+// TODO: Only publish tags for this author:
+/// return Tags.find({authors: Meteor.userId() }); 
+
+Meteor.publish("userData", function () {
+  if (this.userId) {
+    return Meteor.users.find( {_id: this.userId},
+      {fields: 
+        {
+          "services.google.picture": true,
+          "services.google.name": true,
+          "services.facebook.picture": true,
+          "services.facebook.name": true
+        }
+      });
+  } else {
+    this.ready();
+  }
+});
