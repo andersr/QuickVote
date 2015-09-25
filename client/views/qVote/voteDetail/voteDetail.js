15f1d0348ceb8134fa21fc6965f34c5fc62871da
Anders Ramsay <andersr@gmail.com>
2015-09-25 14:08:57 -0400

    cleanup

--- client/views/qVote/voteDetail/voteDetail.js
+++ client/views/qVote/voteDetail/voteDetail.js
@@ -1,40 +1,15 @@
-Template.appHeader.onCreated(function(){
-
-  // var templateInstance  = this;
-
-  // templateInstance.currentVoteId = new ReactiveVar(Router.current().params._id);
-     
-  // templateInstance.autorun(function(){
-
-  //   if (Votes.find({_id: templateInstance.currentVoteId.get()}) == undefined) {
-  //     Router.go('home');
-  //     sAlert.info("This vote has been deleted.");
-  //   };
-
-  // });
-
-});
-
-
-
 Template.voteDetail.helpers({
   
   voteChoices:function(){
-    return VoteChoices.find({voteId: Router.current().params._id }, {sort: {updatedAt: -1, count: -1, }});    
+    return VoteChoices.find({voteId: Router.current().params._id }, {sort: { updatedAt: -1 }});
   }
 });
 
 Template.voteDetail.events({
+
   "submit .vote-title-form": function(event,template){
-    //prevent unwanted default behavior for form submit
     event.preventDefault();
-
-    //allow for exposing 'this' to internal functions, such as Meteor.call
-    // var self = this;
-
-    //get text entered into topicTitle field
     var voteTitle = event.target.voteTitle.value;
-
     var voteAttributes = {
       title: voteTitle
     };
@@ -43,12 +18,9 @@ Template.voteDetail.events({
       if (error){
         console.log(error.reason);
       } else {
-
-         //clear out form data after insert
          template.find("form").reset();
-
       }
     });
-
   }
+
 });
--- client/views/qVote/voteTitle/voteTitle.js
+++
@@ -1,3 +0,0 @@
-// Template.voteTitle.onRendered(function(){
-//   $( "input[name=voteTitle]" ).focus();
-// });
