2c00469aa449acb9e87e5f765766810db52ecd73
Anders Ramsay <andersr@gmail.com>
2015-09-26 17:30:51 -0400

    winning vote in progress

--- client/views/qVote/voteChoices/upDownVote/upDownVote.js
+++ client/views/qVote/voteChoices/upDownVote/upDownVote.js
@@ -1,28 +1,17 @@
 Template.upDownVote.onCreated(function(){
-
-  //allow for making template instance available inside autorun
   var templateInstance = this;
-
-  //associate upvotes with a corresponding vote choice
   templateInstance.currentVoteChoice = new ReactiveVar(templateInstance.data._id);
 
-  //enable determining if we should add a new vote or update an existing vote 
   templateInstance.firstVote = new ReactiveVar(true);
-
-  //track the user vote for this vote choice
   templateInstance.currentVote = new ReactiveVar(false);
 
-
-  //enable auto-checking for updates to this user's votes
   templateInstance.autorun(function(){
 
     var userVotesSubscription = templateInstance.subscribe('userVotes');
 
     if (userVotesSubscription.ready()) {
 
-      // check if user has voted previously
       if (Meteor.userId()) {
-
         var userVote = UserVotes.findOne({
           voteChoiceId:templateInstance.currentVoteChoice.get(),
           voterId: Meteor.userId()
@@ -32,6 +21,7 @@ Template.upDownVote.onCreated(function(){
           templateInstance.currentVote.set(userVote.upVote);
           templateInstance.firstVote.set(false);
         };
+
       } else {
         templateInstance.firstVote.set(true);
         templateInstance.currentVote.set(false);
@@ -67,16 +57,13 @@ Template.upDownVote.events({
 
     var voteChoiceId = Template.instance().currentVoteChoice.get();
     var upVote = Template.instance().currentVote.get();
-    // console.log("upVote on click: " + upVote);
 
-    // if user is not signed in, ask to sign in before voting
     if (!Meteor.userId()) {
       Session.set("loginViaModal", true);
       $('#loginModal').modal('show');
     } else {
-      //if this is their first time voting, create a new userVote for this vote choice
-      if (Template.instance().firstVote.get()) {
 
+      if (Template.instance().firstVote.get()) {
         Meteor.call('newUserVote', voteChoiceId, function (error, result) {
           if (error){
             console.log(error.reason);
@@ -84,8 +71,6 @@ Template.upDownVote.events({
         });
 
       } else {
-        //if not, updating their existing vote choice
-        // console.log("not first vote...");
 
         var userVoteAttributes = {
           voteChoiceId: voteChoiceId,
