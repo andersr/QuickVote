c4b4e30877c9f63ad9daeae193b80dd2989542a7
Anders Ramsay <andersr@gmail.com>
2015-10-03 16:54:46 -0400

    vote winner in progress

--- client/templates/qVote/voteChoices/upDownVote/upDownVote.js
+++ client/templates/qVote/voteChoices/upDownVote/upDownVote.js
@@ -2,7 +2,7 @@ Template.upDownVote.onCreated(function(){
   var templateInstance = this;
 
   templateInstance.currentVoteChoiceId = new ReactiveVar(templateInstance.data._id);
-  templateInstance.previousVote = new ReactiveVar(false);
+  templateInstance.previousVote = new ReactiveVar();
 
   templateInstance.autorun(function(){
 
@@ -11,13 +11,20 @@ Template.upDownVote.onCreated(function(){
     if (userVotesSubscription.ready()) {
 
       if (Meteor.userId()) {
-        var userVote = UserVotes.find({
+
+        var hasVoted = UserVotes.find({
           voteChoiceId:templateInstance.currentVoteChoiceId.get(),
           userId: Meteor.userId()
-        }, {limit: 1});
+        }, {limit: 1}).count() > 0;
 
-        if (userVote.count() > 0) {
-          templateInstance.previousVote.set(userVote.upVote);
+        if (hasVoted) {
+          var upVote = UserVotes.findOne({
+          voteChoiceId:templateInstance.currentVoteChoiceId.get(),
+          userId: Meteor.userId()
+          }).upVote;
+          templateInstance.previousVote.set(upVote);
+        } else {
+          templateInstance.previousVote.set(false);          
         };
       };
     };
@@ -29,6 +36,7 @@ Template.upDownVote.helpers({
 
   thumbIconToggle: function(){
     if (Template.instance().previousVote.get()) {
+      console.log("toggle: " + Template.instance().previousVote.get());
       return "thumbs-up";
     } else {
       return "thumbs-o-up";
@@ -55,11 +63,12 @@ Template.upDownVote.events({
       $('#loginModal').modal('show');
 
     } else {
-
       var firstVote = UserVotes.find({
         userId: Meteor.userId(),
         voteChoiceId:Template.instance().currentVoteChoiceId.get() 
       }).count() === 0;
+
+      // console.log("vote choice id: " + Template.instance().currentVoteChoiceId.get());
 
       var userVoteAttributes = {
         voteChoiceId: Template.instance().currentVoteChoiceId.get()
@@ -77,7 +86,7 @@ Template.upDownVote.events({
 
       } else {
 
-        _.extend(userVoteAttributes, { upVote: !Template.instance().previousVote.get() });
+        // _.extend(userVoteAttributes, { upVote: !Template.instance().previousVote.get() });
 
         Meteor.call('upDownVote', userVoteAttributes, function (error, result) {
           if (error){
