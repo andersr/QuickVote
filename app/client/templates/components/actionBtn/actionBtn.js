00e9dbd80d7ce1db489b827ee7058bdb971693f0
Anders Ramsay <andersr@gmail.com>
2015-10-19 09:38:38 -0400

    contact email link

---
+++ app/client/templates/components/actionBtn/actionBtn.js
@@ -0,0 +1,8 @@
+// data-toggle="tooltip" data-placement="left" title="Tooltip on left"
+
+// Template.actionButton.helpers({
+//   toolTip:function(){
+//     return "data-toggle='tooltip'";
+//   }
+
+// });
---
+++ app/client/templates/components/contactEmail/contactEmail.js
@@ -0,0 +1,9 @@
+// Temlate.contacteEmail.onRendered(function(){
+//    $('.tooltip').tooltip();
+// });
+
+// Template.contactEmail.helpers({
+//   emailAddress: function () {
+//     return AppLib.constants.appContactEmail || "";
+//   }
+// });
