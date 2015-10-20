5f5e4c17bd5dff3970b19acc0bbd02f1b658a633
Anders Ramsay <andersr@gmail.com>
2015-10-20 16:06:40 -0400

    display name of vote creator

---
+++ app/client/templates/qVote/voteMetaInfo/voteMetaInfo.js
@@ -0,0 +1,47 @@
+// Template.voteMetaInfo.onRendered(function(){
+//   $( "input[name=voteTitle]" ).focus();
+// });
+
+Template.voteMetaInfo.helpers({
+  voteCreatorName: function () {
+    var vote = Votes.findOne({_id: Router.current().params._id });
+    // var voteOwner = Votes.findOne({_id: Router.current().params._id }).owner;
+    var owner = Meteor.users.findOne({_id: vote.owner });
+    return AppLib.txtHelpers.firstWordFirstCharOfSecondWord(owner.profile.name);
+    
+    
+  }
+});
+
+Template.voteMetaInfo.events({
+  "submit .new-vote-form": function(event,template){
+    // console.log("submitted new vote");
+    event.preventDefault();
+
+    var voteTitle = event.target.voteTitle.value;
+
+    var voteAttributes = {
+      title: voteTitle
+    };
+
+    Meteor.call('createVote', voteAttributes, function(error, result){
+      if (error){
+        console.log(error.reason);
+      } else {
+        Session.set("newVote", false);
+        Router.go('voteDetail', { _id: result.voteId });
+        Session.set("addVoteChoice", true);
+      }
+    });
+  }
+  ,
+  "keyup input": function(e,t){
+    e.preventDefault();
+
+    if (e.keyCode == 27){
+      Session.set("newVote", false);
+    };
+    e.stopPropagation();
+
+  }
+});
