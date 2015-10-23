55c0f05be2f5c37ae19c2360264707a56809ee09
Anders Ramsay <andersr@gmail.com>
2015-10-23 17:52:50 -0400

    component working with key attribute

--- app/client/templates/components/clickToEdit/clickToEdit.js
+++ app/client/templates/components/clickToEdit/clickToEdit.js
@@ -1,15 +1,29 @@
 Template.clickToEdit.onCreated(function(){
-  // console.log("currentItem: " + this.data.itemLabel);
-  // console.log("id: " + this.data.itemId);
+  // console.log("currentItem: " + this.data.itemName);
+  console.log("title value: " + this.data._id);
+
+
+  // var itemAttributes = {};
+
+  // itemAttributes.itemKey = this.data.itemName; //"title"
+  // itemAttributes.itemValue = this.data.itemAttributes[itemAttributes.itemKey]; //value of title
+
+  //  console.log("item value: " + itemAttributes.itemValue);
+
 
   var
   templateInstance                 = this;
+  templateInstance.itemKey         = new ReactiveVar(this.data.itemKey),
   templateInstance.currentItem     = new ReactiveVar(this.data._id),
   templateInstance.itemName       = new ReactiveVar(this.data.itemName),
   // templateInstance.itemCollection = new ReactiveVar(this.data.itemCollection),
   templateInstance.editing         = new ReactiveVar(false),
   templateInstance.editableItem    = new ReactiveVar(null)
   ;
+
+  // templateInstance.itemAttributes = new ReactiveVar();
+
+  
 
   templateInstance.autorun(function(){
     templateInstance.editing.set(
@@ -47,13 +61,13 @@ Template.clickToEdit.events({
 
   "submit .update-item": function(event, template){
     event.preventDefault();
-    console.log("collection name: " + this.itemCollection);
+    // console.log("collection name: " + this.itemCollection);
 
     var itemAttributes = {};
     // var currentAttribute = Template.instance.itemLabel.get();
 
     itemAttributes.id = this.itemId;
-    itemAttributes.fieldName = "title";
+    itemAttributes.fieldName = this.itemKey;
     itemAttributes.collection = this.itemCollection;
     itemAttributes.fieldValue = template.find('.update-item .item-title').value;
 
