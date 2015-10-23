8ce0ef63fc6f71a3545fda67fc3ba2384338437d
Anders Ramsay <andersr@gmail.com>
2015-10-23 15:53:26 -0400

    get collection name via attribute

--- app/client/templates/components/clickToEdit/clickToEdit.js
+++ app/client/templates/components/clickToEdit/clickToEdit.js
@@ -5,7 +5,8 @@ Template.clickToEdit.onCreated(function(){
   var
   templateInstance                 = this;
   templateInstance.currentItem     = new ReactiveVar(this.data._id),
-  templateInstance.itemLabel     = new ReactiveVar(this.data.itemLabel),
+  templateInstance.itemName       = new ReactiveVar(this.data.itemName),
+  // templateInstance.itemCollection = new ReactiveVar(this.data.itemCollection),
   templateInstance.editing         = new ReactiveVar(false),
   templateInstance.editableItem    = new ReactiveVar(null)
   ;
@@ -29,7 +30,7 @@ Template.clickToEdit.helpers({
   },
   itemLabel: function () {
     // var fieldName = 
-    return this.itemLabel;
+    return this.itemName;
   }
 });
 
@@ -46,14 +47,14 @@ Template.clickToEdit.events({
 
   "submit .update-item": function(event, template){
     event.preventDefault();
-    // console.log("update id: " + this.itemId);
+    console.log("collection name: " + this.itemCollection);
 
     var itemAttributes = {};
     // var currentAttribute = Template.instance.itemLabel.get();
 
     itemAttributes.id = this.itemId;
     itemAttributes.fieldName = "title";
-    itemAttributes.collection = "voteChoices";
+    itemAttributes.collection = this.itemCollection;
     itemAttributes.fieldValue = template.find('.update-item .item-title').value;
 
     AppLib.collectionHelpers.clickToEditUpdate(itemAttributes);
