08f1b634a0ae1c85a3fb8e40137501d1a05dd680
Anders Ramsay <andersr@gmail.com>
2015-10-24 09:47:19 -0400

    cleanup

--- app/client/templates/components/clickToEdit/clickToEdit.js
+++ app/client/templates/components/clickToEdit/clickToEdit.js
@@ -1,32 +1,13 @@
 Template.clickToEdit.onCreated(function(){
-  // console.log("currentItem: " + this.data.itemName);
-  // console.log("title value: " + this.data._id);
-
-
-  // var itemAttributes = {};
-
-  // itemAttributes.itemKey = this.data.itemName; //"title"
-  // itemAttributes.itemValue = this.data.itemAttributes[itemAttributes.itemKey]; //value of title
-
-  //  console.log("item value: " + itemAttributes.itemValue);
-
 
   var
   templateInstance                 = this;
   templateInstance.fieldKey         = new ReactiveVar(this.data.fieldKey),
   templateInstance.itemId     = new ReactiveVar(this.data.itemId),
-
-  // templateInstance.itemCollection = new ReactiveVar(this.data.itemCollection),
   templateInstance.editing         = new ReactiveVar(false),
   templateInstance.editableItem    = new ReactiveVar(null)
   ;
-
-  // templateInstance.itemAttributes = new ReactiveVar();
-
-  // console.log("item id: " + templateInstance.itemId.get());
-
-  
-
+ 
   templateInstance.autorun(function(){
     templateInstance.editing.set(
       templateInstance.itemId.get() === templateInstance.editableItem.get()
@@ -34,18 +15,11 @@ Template.clickToEdit.onCreated(function(){
   });
 });
 
-// Template.clickToEdit.onRendered(function(){
-//     $( "input[name=itemTitle]" ).focus();
-// });
-
-
 Template.clickToEdit.helpers({
   editing: function () {
-    // var fieldName = 
     return Template.instance().editing.get();
   },
   fieldValue: function () {
-    // var fieldName = 
     return this.fieldValue;
   }
 });
@@ -53,7 +27,6 @@ Template.clickToEdit.helpers({
 Template.clickToEdit.events({
 
   'click .edit-item': function () {
-     // console.log("edit id: " + this._id + "itemId: " + this.itemId);
     Template.instance().editableItem.set(Template.instance().itemId.get());
   },
 
@@ -63,11 +36,8 @@ Template.clickToEdit.events({
 
   "submit .click-to-edit-form": function(event, template){
     event.preventDefault();
-    // console.log("collection name: " + this.itemCollection);
 
     var itemAttributes = {};
-    // var currentAttribute = Template.instance.itemLabel.get();
-
     itemAttributes.itemId = this.itemId;
     itemAttributes.fieldKey = this.fieldKey;
     itemAttributes.collection = this.collection;
