7bf6cdf0a705cb36ba74c8069c33c409d617e38e
Anders Ramsay <andersr@gmail.com>
2015-10-27 09:47:21 -0400

    updated userAccounts error messages

--- app/lib/config/at_config.js
+++ app/lib/config/at_config.js
@@ -4,6 +4,16 @@ AccountsTemplates.configure({
   texts: {
     title: {
       signIn: "Sign in with..."
+    },
+    errors: {
+      accountsCreationDisabled: "Sorry, account creation is currently disabled.",
+      cannotRemoveService: "Sorry, can't remove the only active service.",
+      captchaVerification: "Sorry, captcha verification failed.",
+      loginForbidden: "Sorry, your email and/or password were incorrect.",
+      mustBeLoggedIn: "Sorry, you need to be signed in to continue.",
+      pwdMismatch: "Oops, looks like those passwords don't match.",
+      validationErrors: "There was a problem with the info you entered. Please try again.",
+      verifyEmailFirst: "Please check your inbox and verify your email before continuing.",
     }
   }
 });
