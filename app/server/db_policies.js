64c1545999db41967c9226aa4c33eb617fbfef46
Anders Ramsay <andersr@gmail.com>
2015-10-27 17:19:48 -0400

    uservotes policies

--- app/server/db_policies.js
+++ app/server/db_policies.js
@@ -1,43 +1,34 @@
 //Db security/restrictions. See:https://atmospherejs.com/ongoworks/security
-Security.defineMethod('ownsVote', {
+Security.defineMethod('isVoteOwner', {
   fetch: ['owner'],
   deny: function (type, arg, userId, doc) {
     return userId !== doc.owner;
   }
 });
 
+Security.defineMethod('isVoter', {
+  fetch: ['userId'],
+  deny: function (type, arg, userId, doc) {
+    return userId !== doc.userId;
+  }
+});
+
+
+// VOTES Policies
 //Only allow authenticated users to create votes
 Votes.permit(['insert']).ifLoggedIn().apply();
 
 //Only allow the owner of a vote to update or remove it
-Votes.permit(['update', 'remove']).ownsVote().apply();
+Votes.permit(['update', 'remove']).isVoteOwner().apply();
 
-
+// VOTE CHOICES Policies
 //Must be vote owner to add/update/remove a corresponding vote choice
-VoteChoices.permit(['insert', 'update', 'remove']).ownsVote().apply();
+VoteChoices.permit(['insert', 'update', 'remove']).isVoteOwner().apply();
 
+// USER VOTES Policies
 
-// Votes.permit(['update', 'remove']).ifHasUserId(this.userId).apply();
+// Must be authenticated to insert a userVote
+UserVotes.permit(['insert']).ifLoggedIn().apply();
 
-// Votes.allow({  
-//   insert: function (userId, doc) {
-//     return userId;
-//   },
-//   update: function (userId, doc, fields, modifier) {
-//     // can only change your own documents
-//     return doc.userId === userId;
-//   },
-//   remove: function (userId, doc) {
-//     // can only remove your own documents
-//     return doc.userId === userId;
-//   }
-// });
-
-
-// deny anyone that tries to update the document userId:
-// Votes.deny({  
-//   update: function (userId, docs, fields, modifier) {
-//     // can't change owners
-//     return _.contains(fields, 'userId');
-//   }
-// });
+// Must be owner of user vote to update or remove
+UserVotes.permit(['update', 'remove']).isVoter().apply();
