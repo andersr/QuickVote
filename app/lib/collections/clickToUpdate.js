8cde87f63a8fa73829765a6513b4b8e2fbbf819a
Anders Ramsay <andersr@gmail.com>
2015-10-27 16:00:54 -0400

    only owner of a vote choice can edit it

--- app/lib/collections/clickToUpdate.js
+++ app/lib/collections/clickToUpdate.js
@@ -9,12 +9,22 @@
         fieldKey: String,
         fieldValue: String
       });
+
       var dbUpdate = {};
       dbUpdate[updateAttributes.fieldKey] = updateAttributes.fieldValue;
 
-     Mongo.Collection.get(updateAttributes.collection).update(updateAttributes.itemId,{ 
-      $set: dbUpdate
-     });
+      //security hack to prevent non-owner of a voteChoice from updating it
+      if (updateAttributes.collection === 'voteChoices') {
+        var voteChoice = VoteChoices.findOne({_id: updateAttributes.itemId });
+
+        if (voteChoice.owner != Meteor.userId()) {
+          return;
+        };
+      };
+
+      Mongo.Collection.get(updateAttributes.collection).update(updateAttributes.itemId,{ 
+        $set: dbUpdate
+      });
 
     }
   });
